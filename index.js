53abafaa12e137fc52a05cee268be98ca16f4e8b
Gm786h <149281934+Gm786h@users.noreply.github.com>
2023-12-05 19:46:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceImage="dice"+randomNumber1+".png";//dice png 1 to 6
-var randomImageSource="images/"+randomDiceImage;//images.dice1.pnd to dice6.png
+var randomImageSource=randomDiceImage;//images.dice1.pnd to dice6.png
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomDiceImage2="dice"+randomNumber2+".png";//dice png 1 to 6
-var randomImageSource2="images/"+randomDiceImage2;
+var randomImageSource2=randomDiceImage2;
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSource2);
 if(randomNumber1>randomNumber2){
