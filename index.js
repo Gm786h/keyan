acc642f569d9f14d13a4db3b7633fb4c417695b6
Gm786h <149281934+Gm786h@users.noreply.github.com>
2023-12-06 19:38:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
+function play(){
+    
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceImage="dice"+randomNumber1+".png";//dice png 1 to 6
 var randomImageSource=randomDiceImage;//images.dice1.pnd to dice6.png
-var image1=document.querySelectorAll("img")[0];
+var image1=document.querySelectorAll("img")[1];
 image1.setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomDiceImage2="dice"+randomNumber2+".png";//dice png 1 to 6
 var randomImageSource2=randomDiceImage2;
-var image2=document.querySelectorAll("img")[1];
+var image2=document.querySelectorAll("img")[2];
 image2.setAttribute("src",randomImageSource2);
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="&#128681; Player1 Win";
@@ -17,4 +19,5 @@ if(randomNumber1>randomNumber2){
 }
 else{
     document.querySelector("h1").innerHTML="  &#x1F6A9; Player2 Win";
+}
 }
